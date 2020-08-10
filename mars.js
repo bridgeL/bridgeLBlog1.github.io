4689704b8bb3ded6b806b4f3dc46fccca0061cb4
bridgeL <47290820+bridgeL@users.noreply.github.com>
2020-08-10 14:50:41 +0800

    update password

--- mars.js
+++ mars.js
@@ -1,6 +1,6 @@
 function Check () {
     let password = txt.value;
-    if (password == '1') { //666214666
+    if (password == '666214666') { //666214666
         let str = "密码正确 <\hr> ";
         let week1 = "<a href=\"./MarsWeek/第1周/我的朋友们.html\">Mars Week 1</a> <br/>"
         let week2 = "<a href=\"./MarsWeek/第2周/我的朋友们.html\">Mars Week 2</a> <br/>"
